bf4b327388c5c60685c664f9d12922d6f0b3220d
nipun <nipuncod@gmail.com>
2026-03-25 21:56:35 +0530

    Move helper functions to separate module

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import './reset.css'
 import './style.css'
+import { toTitleCase } from './helpers'
 
-function toTitleCase(str: string) {
-  return str
-    .toLowerCase()
-    .split(' ')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(' ')
 }
